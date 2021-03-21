44fc84ce0e4ea4c917aa3e7d443fa70a8e50df54
Shinya Fujino <shf0811@gmail.com>
2021-03-22 01:11:11 +0900

    Validate given url

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import axios from 'axios'
 import meow from 'meow'
 import TurndownService from 'turndown'
+import validUrl from 'valid-url'
 
 const cli = meow()
 
 const url = cli.input[0]
-// validate url here
+if (!validUrl.isWebUri(url)) {
+  console.log(`Not a valid url: ${url}`)
+  process.exit(1)
+}
+
 axios.get(url).then((response) => {
   const html = response.data
   // validate html here
