3f439b5fd9254d409b6660939a453ead4876f00c
Shinya Fujino <shf0811@gmail.com>
2021-03-21 23:03:42 +0900

    Add tsconfig.eslint.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
-    project: './tsconfig.json',
+    project: ['./tsconfig.eslint.json'],
   },
   plugins: ['@typescript-eslint'],
   rules: {},
