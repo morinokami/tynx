1c837c5290837fcfed43e9bcf81108ce3e317f4b
Shinya Fujino <shf0811@gmail.com>
2021-03-23 00:55:31 +0900

    Use page.content() instead of page.evaluate()

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ if (!validUrl.isWebUri(url)) {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
   await page.goto(url)
-  const htmlBody = await page.evaluate(() => document.body.innerHTML)
+  const htmlBody = await page.content()
 
   const turndownService = new TurndownService()
   const md = turndownService.turndown(htmlBody)
