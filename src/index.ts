03655867abd9a228d768b4229049922dac123ea3
Shinya Fujino <shf0811@gmail.com>
2021-03-24 00:36:05 +0900

    Add cursor and make it move

--- src/index.ts
+++ src/index.ts
@@ -41,12 +41,35 @@ if (!validUrl.isWebUri(url)) {
       border: {
         fg: '#f0f0f0',
       },
-      hover: {
-        bg: 'green',
-      },
     },
   })
   screen.append(box)
+  let top = 0
+  let left = 0
+  const cursorOptions = {
+    parent: box,
+    width: 1,
+    height: 1,
+    style: {
+      fg: 'white',
+      bg: 'white',
+    },
+  }
+  let cursor = blessed.box(Object.assign({}, cursorOptions, { top, left }))
+  box.key(['h', 'j', 'k', 'l'], (ch) => {
+    cursor.detach()
+    if (ch === 'j' || ch === 'k') {
+      top += ch === 'j' ? 1 : -1
+      top = top < 0 ? 0 : top
+      top = top > (box.height as number) - 3 ? (box.height as number) - 3 : top
+    } else if (ch === 'h' || ch === 'l') {
+      left += ch === 'l' ? 1 : -1
+      left = left < 0 ? 0 : left
+      left = left > (box.width as number) - 3 ? (box.width as number) - 3 : left
+    }
+    cursor = blessed.box(Object.assign({}, cursorOptions, { top, left }))
+    screen.render()
+  })
   screen.key(['escape', 'q', 'C-c'], async () => {
     await browser.close()
     return process.exit(0)
