b72d9008c9ec527f987d4ecf73eb26383350c607
Shinya Fujino <shf0811@gmail.com>
2021-03-28 01:19:53 +0900

    Add some util functions

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,3 +5,39 @@ export const htmlToMarkdown = (html: string): string => {
   turndownService.remove(['script'])
   return turndownService.turndown(html)
 }
+
+export const extractLink = (text: string): string | null => {
+  const regex = /^\[[\w\s\d]+\]\(((?:\/|https?:\/\/)[\w\d./?=#]+)\)$/
+  const match = text.match(regex)
+  if (match) {
+    return match[1]
+  }
+  return null
+}
+
+export const getClickedChunk = (text: string, position: number): string => {
+  if (position < 0 || position > text.length - 1) {
+    return ''
+  }
+  if (text[position] === ' ') {
+    return ''
+  }
+
+  let start = position
+  while (start > 0) {
+    start -= 1
+    if (text[start] === ' ') {
+      start += 1
+      break
+    }
+  }
+  let end = position
+  while (end < text.length) {
+    end += 1
+    if (text[end] === ' ') {
+      break
+    }
+  }
+
+  return text.substring(start, end)
+}
