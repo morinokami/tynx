4779b7094e9043f85220db92b8afa7ae1ec36b31
Shinya Fujino <shf0811@gmail.com>
2021-04-03 21:17:09 +0900

    Simplify index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,78 +1,18 @@
 #!/usr/bin/env node
 
 import { CLI } from './cli'
-import Renderer from './renderer'
-import Screen from './ui'
-import { htmlToMarkdown, validateUrl } from './lib'
+import { validateUrl } from './lib'
+import * as tynx from './tynx'
 
 const main = async (): Promise<void> => {
   const cli = new CLI()
-  const renderer = await Renderer.init()
-
   if (!cli.url) {
     console.error('URL not specified')
-    await renderer.close()
-    return
   } else if (!validateUrl(cli.url)) {
     console.error(`Not a valid URL: ${cli.url}`)
-    await renderer.close()
-    return
+  } else {
+    tynx.start(cli.url)
   }
-
-  const follow = async (url: string): Promise<void> => {
-    if (url.startsWith('/')) {
-      const { origin } = renderer.url()
-      url = `${origin}${url}`
-    }
-    if (validateUrl(url)) {
-      screen.clear()
-      await renderer.goto(url)
-    }
-    await render()
-  }
-  const reload = async (): Promise<void> => {
-    screen.clear()
-    await renderer.reload()
-    await render()
-  }
-  const goFoward = async (): Promise<void> => {
-    if (renderer.canGoForward()) {
-      screen.clear()
-      await renderer.goForward()
-      await render()
-    }
-  }
-  const goBack = async (): Promise<void> => {
-    if (renderer.canGoBack()) {
-      screen.clear()
-      await renderer.goBack()
-      await render()
-    }
-  }
-  const cleanUp = async (): Promise<void> => {
-    await renderer.close()
-    process.exit(0)
-  }
-  const render = async (): Promise<void> => {
-    const { title, content } = await renderer.evaluate()
-    const md = htmlToMarkdown(content)
-    screen.update(title, md)
-  }
-
-  await renderer.goto(cli.url)
-  const { title, content } = await renderer.evaluate()
-  const md = htmlToMarkdown(content)
-  const screen = new Screen(
-    title,
-    md,
-    follow,
-    reload,
-    goFoward,
-    goBack,
-    cleanUp,
-  )
-
-  screen.run()
 }
 
 main()
---
+++ src/tynx.ts
@@ -0,0 +1,62 @@
+import Renderer from './renderer'
+import Screen from './ui'
+import { htmlToMarkdown, validateUrl } from './lib'
+
+export const start = async (url: string): Promise<void> => {
+  const renderer = await Renderer.init()
+
+  const follow = async (url: string): Promise<void> => {
+    if (url.startsWith('/')) {
+      const { origin } = renderer.url()
+      url = `${origin}${url}`
+    }
+    if (validateUrl(url)) {
+      screen.clear()
+      await renderer.goto(url)
+    }
+    await render()
+  }
+  const reload = async (): Promise<void> => {
+    screen.clear()
+    await renderer.reload()
+    await render()
+  }
+  const goFoward = async (): Promise<void> => {
+    if (renderer.canGoForward()) {
+      screen.clear()
+      await renderer.goForward()
+      await render()
+    }
+  }
+  const goBack = async (): Promise<void> => {
+    if (renderer.canGoBack()) {
+      screen.clear()
+      await renderer.goBack()
+      await render()
+    }
+  }
+  const cleanUp = async (): Promise<void> => {
+    await renderer.close()
+    process.exit(0)
+  }
+  const render = async (): Promise<void> => {
+    const { title, content } = await renderer.evaluate()
+    const md = htmlToMarkdown(content)
+    screen.update(title, md)
+  }
+
+  await renderer.goto(url)
+  const { title, content } = await renderer.evaluate()
+  const md = htmlToMarkdown(content)
+  const screen = new Screen(
+    title,
+    md,
+    follow,
+    reload,
+    goFoward,
+    goBack,
+    cleanUp,
+  )
+
+  screen.run()
+}
