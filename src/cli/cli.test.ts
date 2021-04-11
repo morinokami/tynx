1e49c736fb478285a4f2cd531d86b3f3f1736694
Shinya Fujino <shf0811@gmail.com>
2021-04-11 23:23:22 +0900

    Add cache flag

--- src/cli/cli.test.ts
+++ src/cli/cli.test.ts
@@ -3,6 +3,7 @@ import { CLI } from './cli'
 jest.mock('meow', () =>
   jest.fn(() => ({
     input: ['https://example.com'],
+    flags: { cache: true },
   })),
 )
 
@@ -10,5 +11,10 @@ describe('CLI', () => {
   it('retains the url specified as a command line argument', () => {
     const cli = new CLI()
     expect(cli.url).toBe('https://example.com')
+  })
+
+  it('accepts cache flag', () => {
+    const cli = new CLI()
+    expect(cli.useCache).toBe(true)
   })
 })
--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -3,18 +3,34 @@ import meow from 'meow'
 const usage = `
 	Usage
 	  $ tynx <url>
+
+  Options
+    --cache, -c Cache contents
 `
 
 export class CLI {
   private cli: meow.Result<meow.AnyFlags>
   private _url: string
+  private _useCache: boolean
 
   get url(): string {
     return this._url
   }
 
+  get useCache(): boolean {
+    return this._useCache
+  }
+
   constructor() {
-    this.cli = meow(usage)
+    this.cli = meow(usage, {
+      flags: {
+        cache: {
+          type: 'boolean',
+          alias: 'c',
+        },
+      },
+    })
     this._url = this.cli.input[0]
+    this._useCache = this.cli.flags.cache as boolean
   }
 }
